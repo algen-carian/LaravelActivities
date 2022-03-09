673a9a43621f1994d0baff0621af00afff5ff461
Carian, Algen <201911008@gordoncollege.edu.ph>
2022-03-09 12:44:13 +0800

    ACTIVITY 3 update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,9 +1,9 @@
 <?php
-
+ 
 namespace Database\Seeders;
-
+ 
 use Illuminate\Database\Seeder;
-
+ 
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -13,6 +13,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+         \App\Models\User::factory(10)->create();
+         $this->call([PostSeeder::class]);
     }
 }
